82f42305cb60f532b503326630252323bc30c1a5
Simon Cariou <simon.cariou@epfedu.fr>
2020-05-24 20:42:32 +0200

    made all the vues widescreen for later addition of scroll snap. Added a vue file Compet which will embed a flash game to challenge the guests

--- components/Deroulement.vue
+++ components/Deroulement.vue
@@ -1,54 +1,58 @@
 <template>
-  <section class="hero">
-    <div class="hero-body">
-      <h1 class="title">Deroulement de la journee</h1>
-      <h2 class="subtitle">Explications</h2>
-      <div class="timeline is-centered">
-        <header class="timeline-header">
-          <span class="tag is-medium is-primary">Saut du lit</span>
-        </header>
-        <div class="timeline-item is-primary">
-          <div class="timeline-marker is-primary"></div>
-          <div class="timeline-content">
-            <p class="heading">05/12/2020 - 13h</p>
-            <p>RDV Mairie</p>
+  <section class="hero is-fullheight-with-navbar">
+    <div class="hero-body columns is-multiline">
+      <div class="column is-full">
+        <h1 class="title">Deroulement de la journee</h1>
+        <h2 class="subtitle">Explications</h2>
+      </div>
+      <div class="column is-full">
+        <div class="timeline is-centered">
+          <header class="timeline-header">
+            <span class="tag is-medium is-primary">Saut du lit</span>
+          </header>
+          <div class="timeline-item is-primary">
+            <div class="timeline-marker is-primary"></div>
+            <div class="timeline-content">
+              <p class="heading">05/12/2020 - 13h</p>
+              <p>RDV Mairie</p>
+            </div>
           </div>
+          <div class="timeline-item is-warning">
+            <div class="timeline-marker is-warning is-image is-32x32">
+              <img src="https://bulma.io/images/placeholders/32x32.png" />
+            </div>
+            <div class="timeline-content">
+              <p class="heading">05/12/2020 - 15h</p>
+              <p>En route vers la salle</p>
+            </div>
+          </div>
+          <div class="timeline-item is-danger">
+            <div class="timeline-marker is-danger is-icon">
+              <i class="fa fa-flag"></i>
+            </div>
+            <div class="timeline-content">
+              <p class="heading">05/12/2020 - 18h</p>
+              <p>Debut du cocktail</p>
+            </div>
+          </div>
+          <div class="timeline-item is-primary">
+            <div class="timeline-marker is-primary"></div>
+            <div class="timeline-content">
+              <p class="heading">05/12/2020 - 20h</p>
+              <p>A table !!</p>
+            </div>
+          </div>
+          <div class="timeline-item is-warning">
+            <div class="timeline-marker is-warning"></div>
+            <div class="timeline-content">
+              <p class="heading">05/12/2020 - 22h</p>
+              <p>A partir de la, ca danse</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-medium is-primary">Au dodo</span>
+          </header>
         </div>
-        <div class="timeline-item is-warning">
-          <div class="timeline-marker is-warning is-image is-32x32">
-            <img src="https://bulma.io/images/placeholders/32x32.png" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">05/12/2020 - 15h</p>
-            <p>En route vers la salle</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-danger is-icon">
-            <i class="fa fa-flag"></i>
-          </div>
-          <div class="timeline-content">
-            <p class="heading">05/12/2020 - 18h</p>
-            <p>Debut du cocktail</p>
-          </div>
-        </div>
-        <div class="timeline-item is-primary">
-          <div class="timeline-marker is-primary"></div>
-          <div class="timeline-content">
-            <p class="heading">05/12/2020 - 20h</p>
-            <p>A table !!</p>
-          </div>
-        </div>
-        <div class="timeline-item is-warning">
-          <div class="timeline-marker is-warning"></div>
-          <div class="timeline-content">
-            <p class="heading">05/12/2020 - 22h</p>
-            <p>A partir de la, ca danse</p>
-          </div>
-        </div>
-        <header class="timeline-header">
-          <span class="tag is-medium is-primary">Au dodo</span>
-        </header>
       </div>
     </div>
   </section>
--- components/Photos.vue
+++ components/Photos.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Photos</div>
+  <section class="hero is-fullheight-with-navbar">
+    <div class="hero-body columns is-multiline">
+      <div class="column is-full">
+        <h1 class="title">Photos</h1>
+        <h2 class="subtitle">Explications</h2>
+      </div>
+      <div class="column is-full">
+        <p>BLABLA</p>
+      </div>
+    </div>
+  </section>
 </template>
