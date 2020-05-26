8b6c83a9d34cf1a046bb5a26363ef42862b424f1
Simon Cariou <simon.cariou@epfedu.fr>
2020-05-26 19:28:43 +0200

    set hero-body padding-top to 0 & removed 2 columns in hebergements

--- components/Hebergements.vue
+++ components/Hebergements.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="hebergements" class="hero is-fullheight-with-navbar is-light is-bold">
-    <div class="hero-head">
+  <section class="hero is-fullheight-with-navbar is-light is-bold">
+    <div id="hebergements" class="hero-head">
       <h1 class="title">HEBERGEMENTS</h1>
       <h2 class="subtitle">Liste des endroits ou dormir dans le coin.</h2>
     </div>
@@ -9,32 +9,16 @@
         <div class="columns is-multiline">
           <div class="column is-half-desktop">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns">
-              <div class="column is-half">
+            <div class="columns is-centered is-mobile">
+              <div class="column is-three-quarters-mobile">
                 <img src="https://via.placeholder.com/640x360" />
               </div>
             </div>
           </div>
           <div class="column is-half-desktop">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns">
-              <div class="column is-half">
-                <img src="https://via.placeholder.com/640x360" />
-              </div>
-            </div>
-          </div>
-          <div class="column is-half-desktop">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns">
-              <div class="column is-half">
-                <img src="https://via.placeholder.com/640x360" />
-              </div>
-            </div>
-          </div>
-          <div class="column is-half-desktop">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns">
-              <div class="column is-half">
+            <div class="columns is-centered is-mobile">
+              <div class="column is-three-quarters-mobile">
                 <img src="https://via.placeholder.com/640x360" />
               </div>
             </div>
