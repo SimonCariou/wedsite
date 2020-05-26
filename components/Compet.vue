a55be23c623bf551ee9c9bd65cdecb1824f8ef94
Simon Cariou <simon.cariou@epfedu.fr>
2020-05-26 17:41:03 +0200

    fixed Deroulement layout (columns management, looked crappy on phone)

--- components/Compet.vue
+++ components/Compet.vue
@@ -2,7 +2,7 @@
   <section class="hero is-fullheight-with-navbar is-light is-bold">
     <div class="hero-body columns is-multiline">
       <div class="column is-full">
-        <h1 class="title">Jeu pour la compet'</h1>
+        <h1 class="title">JEU POUR LA COMPET'</h1>
         <h2 class="subtitle">Explications</h2>
       </div>
       <div class="column is-full">
