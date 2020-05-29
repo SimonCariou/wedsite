ea4e5b127fd13b857ae212e270c297f56fbe6969
Simon Cariou <simon.cariou@epfedu.fr>
2020-05-29 14:52:12 +0200

    fixed too big timeline

--- components/Deroulement.vue
+++ components/Deroulement.vue
@@ -5,57 +5,42 @@
       <h2 class="subtitle">Tout ce que vous avez besoin de savoir</h2>
     </div>
     <div class="columns hero-body">
-      <div class="column has-text-left">
-        <div class="columns">
-          <div class="column">
-            <div class="timeline is-centered">
-              <header class="timeline-header">
-                <span class="tag is-medium is-primary">Saut du lit</span>
-              </header>
-              <div class="timeline-item is-primary">
-                <div class="timeline-marker is-primary"></div>
-                <div class="timeline-content">
-                  <p class="heading">05/12/2020 - 13h</p>
-                  <p>RDV Mairie</p>
-                </div>
-              </div>
-              <div class="timeline-item is-warning">
-                <div class="timeline-marker is-warning is-image is-32x32">
-                  <img src="https://bulma.io/images/placeholders/32x32.png" />
-                </div>
-                <div class="timeline-content">
-                  <p class="heading">05/12/2020 - 15h</p>
-                  <p>En route vers la salle</p>
-                </div>
-              </div>
-              <div class="timeline-item is-danger">
-                <div class="timeline-marker is-danger is-icon">
-                  <i class="fa fa-flag"></i>
-                </div>
-                <div class="timeline-content">
-                  <p class="heading">05/12/2020 - 18h</p>
-                  <p>Debut du cocktail</p>
-                </div>
-              </div>
-              <div class="timeline-item is-primary">
-                <div class="timeline-marker is-primary"></div>
-                <div class="timeline-content">
-                  <p class="heading">05/12/2020 - 20h</p>
-                  <p>A table !!</p>
-                </div>
-              </div>
-              <div class="timeline-item is-warning">
-                <div class="timeline-marker is-warning"></div>
-                <div class="timeline-content">
-                  <p class="heading">05/12/2020 - 22h</p>
-                  <p>A partir de la, ca danse</p>
-                </div>
-              </div>
-              <header class="timeline-header">
-                <span class="tag is-medium is-primary">Au dodo</span>
-              </header>
+      <div class="column">
+        <div class="timeline is-centered">
+          <header class="timeline-header">
+            <span class="tag is-medium is-primary">Start</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker is-primary"></div>
+            <div class="timeline-content">
+              <p class="heading">January 2016</p>
+              <p>Timeline content</p>
             </div>
           </div>
+          <div class="timeline-item">
+            <div class="timeline-marker is-warning is-image is-32x32">
+              <img src="https://bulma.io/images/placeholders/32x32.png" />
+            </div>
+            <div class="timeline-content">
+              <p class="heading">February 2016</p>
+              <p>Timeline content</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-primary">2017</span>
+          </header>
+          <div class="timeline-item">
+            <div class="timeline-marker is-danger is-icon">
+              <i class="fa fa-plus"></i>
+            </div>
+            <div class="timeline-content">
+              <p class="heading">March 2017</p>
+              <p>Timeline content</p>
+            </div>
+          </div>
+          <header class="timeline-header">
+            <span class="tag is-medium is-primary">End</span>
+          </header>
         </div>
       </div>
     </div>
