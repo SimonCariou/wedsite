eb6a7989d5e722f2399f9ee250350f2949928445
Simon Cariou <simon.cariou@epfedu.fr>
2020-06-04 18:07:35 +0200

    style typo when switch to mobile mode in hebergements

--- components/Informations.vue
+++ components/Informations.vue
@@ -30,19 +30,27 @@
             <div class="column is-half-desktop is-half-tablet is-full-mobile">
               <div class="card">
                 <div class="card-content">
-                  <div class="content">Comment s'y rendre</div>
+                  <div class="content">
+                    <p>
+                      <strong>COMMENT S'Y RENDRE</strong>
+                      <br />
+                    </p>
+                  </div>
                 </div>
                 <div class="card-image">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.607569389974!2d1.6524822155735763!3d48.97999420009076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6b9b378c1c355%3A0x56036d031fc3031e!2sLE%20VILLAGE%20DE%20SULLY!5e0!3m2!1sen!2sfr!4v1590081892934!5m2!1sen!2sfr"
-                    width="100%"
-                    height="300px"
-                    frameborder="0"
-                    style="border:0;"
-                    allowfullscreen
-                    aria-hidden="false"
-                    tabindex="0"
-                  ></iframe>
+                  <figure class="image is-4by3">
+                    <iframe
+                      class="has-ratio"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.607569389974!2d1.6524822155735763!3d48.97999420009076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6b9b378c1c355%3A0x56036d031fc3031e!2sLE%20VILLAGE%20DE%20SULLY!5e0!3m2!1sen!2sfr!4v1590081892934!5m2!1sen!2sfr"
+                      width="100%"
+                      height="400px"
+                      frameborder="0"
+                      style="border:0;"
+                      allowfullscreen
+                      aria-hidden="false"
+                      tabindex="0"
+                    ></iframe>
+                  </figure>
                 </div>
               </div>
             </div>
