72b64afdea117410d146a6d6a6a3e232bfb43277
Simon Cariou <simon.cariou@epfedu.fr>
2020-06-04 16:06:47 +0200

    card instead of basic paragraphs in Hebergements.vue

--- components/Hebergements.vue
+++ components/Hebergements.vue
@@ -6,20 +6,56 @@
     </div>
     <div class="columns hero-body">
       <div class="column has-text-left">
-        <div class="columns is-multiline">
-          <div class="column is-half-desktop">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns is-centered is-mobile">
-              <div class="column is-three-quarters-mobile">
-                <img src="https://via.placeholder.com/640x360" />
+        <div class="columns is-multiline is-centered is-mobile">
+          <div class="column is-two-fifths-desktop is-half-tablet is-full-mobile">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://cdn.kactus.com/pictures/000/163/601/large/le_village_de_sully_chambre.jpg?1574676985"
+                    alt="Sully"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <p>
+                    <strong>Village de Sully</strong>
+                    <br />
+                    <em>D110 78710 Jouy-Mauvoisin</em>
+                    <br />
+                    <br />
+                    <span>50 couchages</span>
+                    <br />
+                    <em>58e par personne, salle de bain et toilettes sur le palier</em>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div class="column is-half-desktop">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam lorem, venenatis vitae erat at, porta pretium quam.</p>
-            <div class="columns is-centered is-mobile">
-              <div class="column is-three-quarters-mobile">
-                <img src="https://via.placeholder.com/640x360" />
+          <div class="column is-two-fifths-desktop is-half-tablet is-full-mobile">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://cdn.kactus.com/pictures/000/163/601/large/le_village_de_sully_chambre.jpg?1574676985"
+                    alt="Sully"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <p>
+                    <strong>Domaine de l'oree du bois</strong>
+                    <br />
+                    <em>Chemin de la Cote Blanche 78710 Rosny-sur-Seine</em>
+                    <br />
+                    <br />
+                    <span>50 couchages</span>
+                    <br />
+                    <em>68e par personne, salle de bain et toilettes prives.</em>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
--- components/Informations.vue
+++ components/Informations.vue
@@ -2,18 +2,20 @@
   <section id="informations" class="hero is-fullheight-with-navbar">
     <div class="hero-head">
       <h1 class="title is-4">INFORMATIONS</h1>
-      <h2 class="subtitle">Tout ce que vous avez besoin de savoir</h2>
+      <h2 class="subtitle">Tout ce que vous avez besoin de savoir sur le lieu</h2>
     </div>
     <div class="columns hero-body">
       <div class="column has-text-left">
         <div class="columns">
           <div class="column">
-            <p>adresse</p>
+            <p>D110 78710 Jouy-Mauvoisin dans les Yvelines</p>
+            <p>Petit village Français privatise</p>
             <figure class="image is-16by9">
               <img src="@/components/images/sully.jpg" alt="Sully" />
             </figure>
           </div>
           <div class="column">
+            <p>Comment s'y rendre ?</p>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.607569389974!2d1.6524822155735763!3d48.97999420009076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6b9b378c1c355%3A0x56036d031fc3031e!2sLE%20VILLAGE%20DE%20SULLY!5e0!3m2!1sen!2sfr!4v1590081892934!5m2!1sen!2sfr"
               width="100%"
