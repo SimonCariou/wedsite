395010bb4ebca89992f21166bc4bfef8cfc05791
Simon Cariou <simon.cariou@epfedu.fr>
2020-06-10 10:48:57 +0200

    init: HTML bubble pop game

--- components/Compet.vue
+++ components/Compet.vue
@@ -1,12 +1,6 @@
 <template>
-  <section class="hero is-fullheight-with-navbar">
-    <div class="hero-head">
-      <h1 class="title is-4">JEU POUR LA COMPET'</h1>
-      <h2 class="subtitle">Explications</h2>
-    </div>
-    <div class="hero-body columns is-multiline">
-      <div class="column is-full">
-        <!-- Place this code where you'd like the game to appear -->
+  <div class="hero is-fullheight-with-navbar">
+    <!-- Place this code where you'd like the game to appear 
         <iframe
           src="//www.miniclip.com/games/penguin-push/fr/webgame.php?bodybg=5&amp;width=100%&amp;height=408&amp;forcecredits=null"
           width="100%"
@@ -16,7 +10,46 @@
           style="border:none;"
           allowfullscreen
         ></iframe>
-      </div>
-    </div>
-  </section>
+    -->
+    <canvas></canvas>
+  </div>
 </template>
+
+<style type="text/css">
+body {
+  margin: 0;
+  padding: 0;
+  background: #000;
+}
+canvas {
+  display: block;
+  margin: 0 auto;
+  background: #fff;
+}
+</style>
+
+<script>
+export default {
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content:
+          'width=device-width, initial-scale=1 user-scalable=no, maximum-scale=1, user-scalable=0" '
+      },
+      {
+        name: "apple-mobile-web-app-capable",
+        content: "yes"
+      },
+      {
+        name: "apple-mobile-web-app-status-bar-style",
+        content: "black-translucent"
+      },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: "description", name: "description", content: "Meta description" }
+    ]
+  }
+};
+</script>
