c34c08f3c6f43560a80b63ad69b9490fd7017930
Simon Cariou <simon.cariou@epfedu.fr>
2020-06-26 23:46:52 +0200

    font and size modif

--- components/2_Informations.vue
+++ components/2_Informations.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="informations" class="hero is-fullheight-with-navbar">
     <div class="hero-head observedElement">
-      <h1 class="title is-size-3-mobile is-size-1-tablet">Informations</h1>
+      <h1 class="title is-size-2-mobile is-size-1-tablet">Informations</h1>
       <h2
         class="subtitle is-size-5-mobile is-size-4-tablet"
       >Tout ce que vous avez besoin de savoir sur le lieu</h2>
@@ -52,7 +52,7 @@
               </div>
               <div class="card-image">
                 <figure class="image is-16by9">
-                  <iframe
+                  <!-- <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.607569389974!2d1.6524822155735763!3d48.97999420009076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6b9b378c1c355%3A0x56036d031fc3031e!2sLE%20VILLAGE%20DE%20SULLY!5e0!3m2!1sen!2sfr!4v1590081892934!5m2!1sen!2sfr"
                     frameborder="0"
                     style="border:0;"
@@ -60,7 +60,7 @@
                     tabindex="0"
                     allowfullscreen
                     class="has-ratio"
-                  ></iframe>
+                  ></iframe>-->
                 </figure>
               </div>
             </div>
