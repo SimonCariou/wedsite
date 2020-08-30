640247ed4638e9dcd1cdb9c038ef691ca5eae523
Simon Cariou <simon.cariou@epfedu.fr>
2020-08-30 18:37:24 +0200

    peaufinement

--- components/1_2_News.vue
+++ components/1_2_News.vue
@@ -5,7 +5,7 @@
         <h1 class="title is-size-2-mobile is-size-1-tablet">News</h1>
         <h2
           class="subtitle is-size-5-mobile is-size-4-tablet"
-        >Ici vous pouvez suivre et avoir les dernières infos toutes chaudes!</h2>
+        >Ici vous pouvez suivre et avoir les dernières nouvelles toutes chaudes!</h2>
       </div>
     </div>
     <div class="hero-body observedElement">
