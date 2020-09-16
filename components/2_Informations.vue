f5e6cc22857db587e66949cd8405bdfb5c230b84
Simon Cariou <simon.cariou@epfedu.fr>
2020-09-16 18:30:19 +0200

    replaced the google maps iframe by an image to reduce requests when loading the page

--- components/2_Informations.vue
+++ components/2_Informations.vue
@@ -70,8 +70,8 @@
               </div>
               <div class="card-image">
                 <figure class="image is-16by9">
-                  <!-- <img src="@/assets/images/informations/map_sully.jpg" alt="Sully carte" /> -->
-                  <iframe
+                  <img src="@/assets/images/informations/map_sully.jpg" alt="Sully carte" />
+                  <!-- <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2618.607569389974!2d1.6524822155735763!3d48.97999420009076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6b9b378c1c355%3A0x56036d031fc3031e!2sLE%20VILLAGE%20DE%20SULLY!5e0!3m2!1sen!2sfr!4v1598804770258!5m2!1sen!2sfr"
                     frameborder="0"
                     style="border:0;"
@@ -79,7 +79,7 @@
                     tabindex="0"
                     allowfullscreen
                     class="has-ratio"
-                  ></iframe>
+                  ></iframe>-->
                 </figure>
               </div>
             </div>
