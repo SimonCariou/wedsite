1a18bf876b0b586d51144c8d47ec7f8f542e73ce
Simon Cariou <simon.cariou@epfedu.fr>
2021-01-23 14:44:24 +0100

    added panorama viewer. TODO: perf enhancement because heavy image + find better pic.

--- pages/Maurice.vue
+++ pages/Maurice.vue
@@ -1,17 +1,48 @@
 <template>
   <div>
     <WelcomeMru />
+    <InfosMru />
   </div>
 </template>
 
 <script>
 import WelcomeMru from "~/components/Maurice/Welcome_mru.vue";
+import InfosMru from "~/components/Maurice/Infos_mru.vue";
 
 export default {
   components: {
     WelcomeMru,
+    InfosMru,
   },
   layout: "maurice_layout",
+  data: () => ({ observerTitle: null, observerBody: null, intersected: false }),
+  mounted() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        for (var i in entries) {
+          entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("in-view");
+            } else {
+              //entry.target.classList.remove("in-view");
+            }
+          });
+        }
+      },
+      {
+        threshold: 0.25,
+      }
+    );
+
+    var items = this.$el.querySelectorAll(".observedElement");
+    for (var i in items) {
+      if (!items.hasOwnProperty(i)) {
+        continue;
+      }
+
+      this.observer.observe(items[i]);
+    }
+  },
 };
 </script>
 
