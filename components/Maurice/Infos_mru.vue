2bba6fd9ca641a54a9eaec8a465c25b0bc1d7471
Simon Cariou <simon.cariou@epfedu.fr>
2021-02-01 18:37:21 +0100

    font color matching clear is-info

--- components/Maurice/Infos_mru.vue
+++ components/Maurice/Infos_mru.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero is-fullheight-with-navbar is-warning">
+  <section class="hero is-fullheight-with-navbar is-info">
     <div class="hero-head has-text-centered observedElement section-title">
       <div class="container">
         <h1 class="title is-size-2-mobile is-size-1-tablet">
