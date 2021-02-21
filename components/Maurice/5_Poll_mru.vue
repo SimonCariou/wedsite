59e4977a7cec2cfbbf63f7b3abb9b001f96183b2
Simon Cariou <simon.cariou@epfedu.fr>
2021-02-21 17:53:09 +0100

    firebase db support + form styling

--- components/Maurice/5_Poll_mru.vue
+++ components/Maurice/5_Poll_mru.vue
@@ -11,13 +11,54 @@
       </div>
     </div>
     <div class="hero-body observedElement section-body">
-      <div class="container">POLL:</div>
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-5">
+            <form v-on:submit.prevent="add">
+              <label class="label has-text-success">RSVP</label>
+              <div class="field is-grouped">
+                <p class="control is-expanded">
+                  <input
+                    class="input is-success"
+                    type="text"
+                    placeholder="Nom/Prénom"
+                    v-model="text.text"
+                  />
+                </p>
+                <p class="control">
+                  <button type="submit" class="button is-success">
+                    Envoyer
+                  </button>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import { db } from "~/plugins/firebase.js";
+
+let namesRef = db.ref("names");
+
 export default {
-  mounted() {},
+  name: "app",
+  firebase: { texts: namesRef },
+  data() {
+    return {
+      text: {
+        text: "",
+      },
+    };
+  },
+  methods: {
+    add() {
+      namesRef.push(this.text);
+      this.text.text = "";
+    },
+  },
 };
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,24 @@ export default {
         defaultLocale: 'fr',
         strategy: 'prefix',
         lazy: true,
-      }]
+      }],
+    // [
+    //   '@nuxtjs/firebase',
+    //   {
+    //     config = {
+    //       apiKey: "AIzaSyDLjsUfFbdqlDjAUPWcu2e5gogrMmjdqOE",
+    //       authDomain: "wedsite-8b590.firebaseapp.com",
+    //       databaseURL: "https://wedsite-8b590-default-rtdb.firebaseio.com",
+    //       projectId: "wedsite-8b590",
+    //       storageBucket: "wedsite-8b590.appspot.com",
+    //       messagingSenderId: "1087430729253",
+    //       appId: "1:1087430729253:web:0a9dab643fe8436200a9cc"
+    //     },
+    //     services: {
+    //       database: true
+    //     }
+    //   }
+    // ]
   ],
 
   /*
