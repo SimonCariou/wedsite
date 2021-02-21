264cad32cce664093a924a7f220bab1d8d5f8691
Simon Cariou <simon.cariou@epfedu.fr>
2021-02-21 20:19:42 +0100

    better looking wave in RSVP section divider

--- components/Maurice/5_RSVP_mru.vue
+++ components/Maurice/5_RSVP_mru.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="hero is-fullheight-with-navbar is-info" id="rsvp">
-    <div style="height: 150px; overflow: hidden">
+    <!-- <div style="height: 50px; overflow: hidden">
       <svg
         viewBox="0 0 500 150"
         preserveAspectRatio="none"
@@ -11,7 +11,23 @@
           style="stroke: none; fill: #faf0d7"
         ></path>
       </svg>
-    </div>
+    </div> -->
+    <svg
+      viewBox="0 0 1440 200"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <path
+        fill="rgba(250, 240, 215, 1)"
+        d="M 0 175 C 310.8 175 466.2 128 777 128 L 777 128 L 777 0 L 0 0 Z"
+        stroke-width="0"
+      ></path>
+      <path
+        fill="rgba(250, 240, 215, 1)"
+        d="M 776 128 C 1041.6 128 1174.4 180 1440 180 L 1440 180 L 1440 0 L 776 0 Z"
+        stroke-width="0"
+      ></path>
+    </svg>
     <div class="hero-head has-text-centered observedElement section-title">
       <div class="container">
         <h1 class="title is-size-2-mobile is-size-1-tablet is-uppercase">
