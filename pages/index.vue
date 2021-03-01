e3f9e8f40f7864b2a53286ef153b21876e80c881
Simon Cariou <simon.cariou@epfedu.fr>
2021-03-01 19:01:52 +0100

    reduce firebase modules loaded to strict minimum

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,4 @@
 <template>
-  <!-- <section class="hero is-fullheight-with-navbar">
-    <div class="hero-body">
-      <div class="container">
-        <div class="is-grouped buttons is-centered">
-          <button class="button is-light">
-            <nuxt-link :to="localePath('France')">{{
-              $t("pages.global.index.btn_fr")
-            }}</nuxt-link>
-          </button>
-
-          <button class="button is-light">
-            <nuxt-link :to="localePath('Maurice')">{{
-              $t("pages.global.index.btn_mru")
-            }}</nuxt-link>
-          </button>
-        </div>
-        <p class="has-text-centered">
-          {{ $t("pages.global.index.lang_selection") }}
-        </p>
-        <div class="is-grouped buttons is-centered">
-          <button class="button is-light">
-            <nuxt-link
-              v-for="locale in $i18n.locales"
-              v-if="locale.code !== $i18n.locale"
-              :key="locale.code"
-              :to="switchLocalePath(locale.code)"
-            >
-              ➡️ {{ locale.name }}</nuxt-link
-            >
-          </button>
-        </div>
-
-        
-      </div>
-    </div>
-  </section> -->
-
   <section class="hero is-fullheight">
     <span class="lang_sticky">
       {{ $t("pages.global.index.lang_selection") }}
