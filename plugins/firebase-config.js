d92e576507ca1348f6f544f7b5184f6d591ae990
Simon Cariou <simon.cariou@epfedu.fr>
2021-03-01 12:05:09 +0100

    add firebase config to the repo.

---
+++ plugins/firebase-config.js
@@ -0,0 +1,9 @@
+export var config = {
+    apiKey: "AIzaSyDLjsUfFbdqlDjAUPWcu2e5gogrMmjdqOE",
+    authDomain: "wedsite-8b590.firebaseapp.com",
+    databaseURL: "https://wedsite-8b590-default-rtdb.firebaseio.com",
+    projectId: "wedsite-8b590",
+    storageBucket: "wedsite-8b590.appspot.com",
+    messagingSenderId: "1087430729253",
+    appId: "1:1087430729253:web:0a9dab643fe8436200a9cc"
+}
