4041b5eb6e05581ae548de663eaf37c31f7f0933
Simon Cariou <simon.cariou@softbankrobotics.com>
2021-06-30 15:30:10 +0200

    modal background css gradient styling: sunset vibe

--- components/Modal.vue
+++ components/Modal.vue
@@ -3,7 +3,7 @@
         <div class="modal" ref="modal_vaccine">
             <div class="modal-background" @click="close_modal"></div>
             <div class="modal-content">
-                <div class="box">Vaccin Obligatoire pour rentrer sur le territoire !</div>
+                <div class="box modal-content-styling">Vaccin Obligatoire pour rentrer sur le territoire !</div>
             </div>
             <button class="modal-close is-large" aria-label="close" @click="close_modal"></button>
         </div>
@@ -29,6 +29,11 @@ export default {
 </script>
 
 <style scoped lang="sass">
+.modal-content-styling
+    margin: 0 20px
 
+.modal-background
+    background-image: radial-gradient(circle, rgba(231,207,58,0.3) 0%, rgba(214,117,19,0.3) 34%, rgba(74,130,231,0.3) 99%)
+    background-color: rgba(10, 10, 10, 0.7)
 </style>
 
